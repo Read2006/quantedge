0b24953ef0b33c6ba492937ca84dd3dea63f3966
Azan zafar <azanz1439@gmail.com>
2026-09-12 12:22:08 +0500

    Fix export syntax in funding.js

--- api/funding.js
+++ api/funding.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+.export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   const symbols = ['BTCUSDT','ETHUSDT','SOLUSDT','BNBUSDT','DOGEUSDT','ADAUSDT'];
   try {
