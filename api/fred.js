9a8c14c36e26216ae2ee653ea0253098087c776e
Azan zafar <azanz1439@gmail.com>
2026-09-12 12:21:24 +0500

    Fix export syntax in fred.js

--- api/fred.js
+++ api/fred.js
@@ -1,4 +1,4 @@
-.export default async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   const { series } = req.query;
   if (!series) return res.status(400).json({ error: 'series required' });
